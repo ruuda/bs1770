3104083dad63d461226b2ade3eb8b4f95d4143ee
Ruud van Asseldonk <dev@veniogames.com>
2020-08-23 12:44:46 +0200

    Make album loudness analyzer a bit more structured
    I want to extend this so it can write tags as well, storing the results
    in a structured format is helpful for that.

--- examples/main.rs
+++ examples/main.rs
@@ -7,10 +7,86 @@
 
 extern crate bs1770;
 extern crate claxon;
-extern crate hound;
 
-fn analyze_file(fname: &str) -> claxon::Result<bs1770::Windows100ms<Vec<bs1770::Power>>> {
-    let mut reader = claxon::FlacReader::open(fname)?;
+use std::fs;
+use std::io::{Read, Seek, Write};
+use std::io;
+use std::path::PathBuf;
+
+use claxon::FlacReader;
+use bs1770::{Power, Windows100ms};
+
+/// Loudness measurement for a track, and the flac reader that wraps the file.
+struct TrackResult {
+    reader: FlacReader<fs::File>,
+    windows: Windows100ms<Vec<Power>>,
+    gated_power: Power,
+}
+
+/// Loudness measurement for a collection of tracks.
+struct AlbumResult {
+    /// File name, loudness, and original reader, for each track.
+    tracks: Vec<(PathBuf, Power, FlacReader<fs::File>)>,
+
+    /// Loudness for all tracks concatenated.
+    gated_power: Power,
+}
+
+impl AlbumResult {
+    fn print(&self) {
+        for &(ref path, track_gated_power, ref _reader) in &self.tracks {
+            println!(
+                "{:>5.1} LKFS  {}",
+                track_gated_power.loudness_lkfs(),
+                path
+                    .file_name()
+                    .expect("We decoded this file, it should have a name.")
+                    .to_string_lossy(),
+            );
+        }
+        println!(
+            "{:>5.1} LKFS  ALBUM",
+            self.gated_power.loudness_lkfs(),
+        );
+    }
+}
+
+/// Measure loudness of an album.
+fn analyze_album(paths: Vec<PathBuf>) -> claxon::Result<AlbumResult> {
+    let mut windows = Windows100ms::new();
+    let mut tracks = Vec::with_capacity(paths.len());
+
+    for path in paths {
+        // Clear the current line, overwite it with the new message.
+        eprint!("\x1b[2K\rAnalyzing {} ...", path.to_string_lossy());
+        io::stderr().flush()?;
+
+        let file = fs::File::open(&path)?;
+        let track_result = match analyze_file(file) {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("Error while analyzing {}: {}", path.to_string_lossy(), e);
+                return Err(e);
+            }
+        };
+        windows.inner.extend(track_result.windows.inner);
+        tracks.push((path, track_result.gated_power, track_result.reader));
+    }
+
+    // Clear the current line again.
+    eprint!("\x1b[2K\r");
+
+    let result = AlbumResult {
+        tracks: tracks,
+        gated_power: bs1770::gated_mean(windows.as_ref()),
+    };
+
+    Ok(result)
+}
+
+/// Measure loudness of a single track.
+fn analyze_file(file: fs::File) -> claxon::Result<TrackResult> {
+    let mut reader = claxon::FlacReader::new(file)?;
 
     let streaminfo = reader.streaminfo();
     // The maximum amplitude is 1 << (bits per sample - 1), because one bit
@@ -36,24 +112,67 @@ fn analyze_file(fname: &str) -> claxon::Result<bs1770::Windows100ms<Vec<bs1770::
         meters[0].as_100ms_windows(),
         meters[1].as_100ms_windows(),
     );
-    let loudness_lkfs = bs1770::gated_mean(zipped.as_ref()).loudness_lkfs();
-    println!("{:.3} LKFS  {}", loudness_lkfs, fname);
 
-    Ok(zipped)
+    let result = TrackResult {
+        gated_power: bs1770::gated_mean(zipped.as_ref()),
+        windows: zipped,
+        reader: reader,
+    };
+
+    Ok(result)
 }
 
-fn main() {
-    let mut album_windows = bs1770::Windows100ms::new();
+/// Return the start offset and length of the VORBIS_COMMENT block in the file.
+///
+/// The start position and length do include the 4-byte block header.
+fn get_vorbis_comment_location(file: &mut fs::File) -> io::Result<Option<(u64, u64)>> {
+    let mut reader = io::BufReader::new(file);
 
-    // Skip the name of the binary itself.
-    for fname in std::env::args().skip(1) {
-        match analyze_file(&fname[..]) {
-            Ok(mut track_windows) => album_windows.inner.extend(track_windows.inner.drain(..)),
-            Err(e) => eprintln!("Failed to analyze {}: {:?}", fname, e),
+    // The first 4 bytes are the flac header.
+    let mut buf = [0_u8; 4];
+    reader.read_exact(&mut buf[..])?;
+    assert_eq!(&buf, b"fLaC");
+
+    let mut is_last = false;
+
+    while !is_last {
+        // This is a block start boundary, remember the current offset.
+        let pos = reader.seek(io::SeekFrom::Current(0))?;
+
+        // The block header is four bytes, one byte where the first bit
+        // specifies whether this is the last block, and the next 7 bits specify
+        // the block type. Then follows a 24-bit big-endian block length.
+        reader.read_exact(&mut buf[..])?;
+        is_last = (buf[0] >> 7) == 1;
+        let block_type = buf[0] & 0b0111_1111;
+        let is_vorbis_comment = block_type == 4;
+        let block_length = 0
+            | ((buf[1] as u64) << 16)
+            | ((buf[2] as u64) << 8)
+            | ((buf[3] as u64) << 0)
+            ;
+
+        if is_vorbis_comment {
+            return Ok(Some((pos, block_length)));
+        } else {
+            reader.seek(io::SeekFrom::Current(block_length as i64))?;
         }
     }
 
-    let album_loudness_lkfs = bs1770::gated_mean(album_windows.as_ref()).loudness_lkfs();
-    println!("{:.3} LKFS  Album", album_loudness_lkfs);
+    Ok(None)
+}
+
+fn main() {
+    // Skip the name of the binary itself.
+    let fnames = std::env::args().skip(1).map(PathBuf::from).collect();
+    let album_result = match analyze_album(fnames) {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("Failed to analzye album: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    album_result.print();
 }
 
