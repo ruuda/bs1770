5d29bb56b7fdb801551977558fc9c897bcad094c
Ruud van Asseldonk <dev@veniogames.com>
2020-08-24 22:43:11 +0200

    Fix bug in enumerating leaf directories with find
    This is what I get for not wanting to write a Python or Rust script to
    do this, and then trusting a hack from the interwebs because find doen't
    have a way to list leaves.
    
    There was a bug where directories that are a prefix of a sibling
    directory did not get matched. For example
    
        /music/Toto/Toto IV
        /music/Toto/Toto                  <-- Not matched
        /Music/Various Artists/Alchemy 2
        /Music/Various Artists/Alchemy    <-- Not matched
    
    Fix this by not caring about whether a directory is a leaf or not. To
    stop find from recursively finding files, add a -maxdepth of 1.

--- tag_collection.sh
+++ tag_collection.sh
@@ -9,17 +9,14 @@ set -e
 # the full path to it.
 flacgain=$(realpath target/release/examples/flacgain)
 
-# List all directories, then filter that down to leaves only with sort and awk.
-# For each directory, run the second find command, running up to the number of
-# cores in parallel (so we process albums in parallel). The find command lists
-# all flac files in the directory, and runs the "flacgain" binary once per
-# album, passing all found flac files as arguments (the + triggers this
-# behavior). For the first "{}", `parallel` fills in the directory name. For the
-# second "{}", `find` fills in the file name, and that one needs to be escaped
-# to prevent `parallel` from substituting it.
+# List all directories. For each directory, run the second find command, running
+# up to the number of cores in parallel (so we process albums in parallel). The
+# find command lists all flac files in the directory, and runs the "flacgain"
+# binary once per album, passing all found flac files as arguments (the +
+# triggers this behavior). For the first "{}", `parallel` fills in the directory
+# name. For the second "{}", `find` fills in the file name, and that one needs
+# to be escaped to prevent `parallel` from substituting it.
 find $1 -type d \
-  | sort -r \
-  | awk 'index(a, $0) != 1 { a = $0; print }' \
   | parallel --bar --jobs '+8' \
-    find {} -type f -name '*.flac' -execdir \
+    find {} -type f -name '*.flac' -maxdepth 1 -execdir \
     "${flacgain}" --skip-when-tags-present --write-tags '\{\}' '+'
